00a9cedeb10da94b9a4defb5c24b2e949e409918
François <f.deryckel@gmail.com>
2018-01-23 14:59:24 +0300

    Add files via upload

---
+++ get_data.r
@@ -0,0 +1,78 @@
+library(tidyverse)
+
+portfolio <- read_csv("~/Google Drive/Software/R projects/shinyapps/Portfolio/transaction.csv") 
+
+startDate = "2004-01-01"
+thePath = "~/Google Drive/Software/R projects/shinyapps/Portfolio/"
+
+######################################################################
+## Alpha Vantage for Stocks from American Exchange  
+######################################################################
+av <- portfolio %>% 
+  select(tickerb = saved_as, ticker = alpha_ticker) %>% na.omit() %>% distinct()
+
+
+for (i in 1:nrow(av)){
+  print(av$ticker[i])
+  data = quantmod::getSymbols(Symbols = av$ticker[i],
+                              src = "av",
+                              api.key = "W7SHG93NFG5YWE2K",
+                              output.size = "full", 
+                              from = startDate,
+                              auto.assign = FALSE,
+                              index.class="POSIXct")
+  colnames(data) = c("Open", "High", "Low", "Adjusted", "Volume")
+  zoo::write.zoo(data, paste0(thePath, "financial_data/", av$tickerb[i], ".csv"), 
+                 sep = ",", row.names = FALSE)
+}
+
+
+##### Doing the same for options - need to use the undelying instrument #### 
+options <- portfolio %>% filter(instrument_type == "Options") %>% select(ticker) %>% 
+  as_vector() %>% 
+  str_replace_all("\ .*", "") %>% as_tibble() %>% select(ticker = value)
+
+for (i in 1:nrow(options)){
+  print(options$ticker[i])
+  data = quantmod::getSymbols(Symbols = options$ticker[i],
+                              src = "av",
+                              api.key = "W7SHG93NFG5YWE2K",
+                              output.size = "full", 
+                              from = startDate,
+                              auto.assign = FALSE,
+                              index.class="POSIXct")
+  colnames(data) = c("Open", "High", "Low", "Adjusted", "Volume")
+  zoo::write.zoo(data, paste0(thePath, "financial_data/", options$ticker[i], ".csv"), 
+                 sep = ",", row.names = FALSE)
+}
+
+
+
+
+
+
+
+######################################################################
+## Yahoo for european etf
+######################################################################
+portfolio <- read_csv("~/Google Drive/Software/R projects/shinyapps/Portfolio/transaction.csv") 
+startDate = "2004-01-01"
+thePath = "~/Google Drive/Software/R projects/shinyapps/Portfolio/"
+
+yahoo <- portfolio %>% 
+  select(ticker, yahoo_ticker) %>% na.omit()
+
+for (i in 1:nrow(yahoo)){
+  print(yahoo$ticker[i])
+  data = quantmod::getSymbols(Symbols = yahoo$yahoo_ticker[i],
+                              src = "yahoo",
+                              from = startDate,
+                              auto.assign = FALSE,
+                              index.class="POSIXct")
+  colnames(data) = c("Open", "High", "Low", "Close", "Volume", "Adjusted")
+  zoo::write.zoo(data, paste0(thePath, "financial_data/", yahoo$ticker[i], ".csv"), 
+                 sep = ",", row.names = FALSE)
+}
+
+
+
