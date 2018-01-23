7e1444a0f53165cbe5e63e7c522aa0b709ae9ea4
François <f.deryckel@gmail.com>
2018-01-23 15:25:48 +0300

    Update get_data.r

--- get_data.r
+++ get_data.r
@@ -1,9 +1,9 @@
 library(tidyverse)
 
-portfolio <- read_csv("~/Google Drive/Software/R projects/shinyapps/Portfolio/transaction.csv") 
+portfolio <- read_csv("transaction.csv")
 
 startDate = "2004-01-01"
-thePath = "~/Google Drive/Software/R projects/shinyapps/Portfolio/"
+
 
 ######################################################################
 ## Alpha Vantage for Stocks from American Exchange  
@@ -16,13 +16,13 @@ for (i in 1:nrow(av)){
   print(av$ticker[i])
   data = quantmod::getSymbols(Symbols = av$ticker[i],
                               src = "av",
-                              api.key = "PUT YOUR OWN HERE :-) . ",
+                              api.key = "PUT YOUR OWN KEY HERE ;-) ",
                               output.size = "full", 
                               from = startDate,
                               auto.assign = FALSE,
                               index.class="POSIXct")
   colnames(data) = c("Open", "High", "Low", "Adjusted", "Volume")
-  zoo::write.zoo(data, paste0(thePath, "financial_data/", av$tickerb[i], ".csv"), 
+  zoo::write.zoo(data, paste0("financial_data/", av$tickerb[i], ".csv"), 
                  sep = ",", row.names = FALSE)
 }
 
@@ -30,28 +30,18 @@ for (i in 1:nrow(av)){
 ##### Doing the same for options - need to use the undelying instrument #### 
 options <- portfolio %>% filter(instrument_type == "Options") %>% select(ticker) %>% 
   as_vector() %>% 
-  str_replace_all("\ .*", "") %>% as_tibble() %>% select(ticker = value)
+  str_replace_all("\ .*", "") %>% as_tibble() %>% select(ticker = value) %>% unique()
 
 for (i in 1:nrow(options)){
   print(options$ticker[i])
   data = quantmod::getSymbols(Symbols = options$ticker[i],
                               src = "av",
-                              api.key = "W7SHG93NFG5YWE2K",
+                              api.key = "PUT YOUR OWN KEY HERE ;-) ",
                               output.size = "full", 
                               from = startDate,
                               auto.assign = FALSE,
                               index.class="POSIXct")
   colnames(data) = c("Open", "High", "Low", "Adjusted", "Volume")
-  zoo::write.zoo(data, paste0(thePath, "financial_data/", options$ticker[i], ".csv"), 
+  zoo::write.zoo(data, paste0("financial_data/", options$ticker[i], ".csv"), 
                  sep = ",", row.names = FALSE)
 }
-
-
-
-
-
-
-
-
-
-
